b63dbb52669516ddbd6e233735a313b54716e2e0
regulusb <regulusb@ga-j5.msk.21-school.ru>
2022-11-09 15:59:43 +0300

    add hashtag description

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -8,14 +8,14 @@
 // %[flags][width][.precision][length]specifier
 
 typedef struct {
-    bool minus; // Left-justify within the given field width
-    bool plus; // Forces to precede the result with a plus or minus sign (+ or -) even for positive numbers
-    bool space; // If no sign is going to be written, a blank space is inserted before the value
-    bool hashtag;
-    bool zero; // Left-pads the number with zeroes (0) instead of spaces
-    int width; // (number) - minimum number of character to be printed   or *
+    bool minus;    // Left-justify within the given field width
+    bool plus;     // Forces to precede the result with a plus or minus sign (+ or -) even for positive numbers
+    bool space;    // If no sign is going to be written, a blank space is inserted before the value
+    bool hashtag;  // Made some math things with some specifiers
+    bool zero;     // Left-pads the number with zeroes (0) instead of spaces
+    int width;     // (number) - minimum number of character to be printed   or *
     int precision; // Precision
-    char length; // h, l or L
+    char length;   // h, l or L
     int specifier; // just specifier
 } flags;
 
