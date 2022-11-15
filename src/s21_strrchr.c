3ba4009da372c1b1d9a66e0b6a0077433a0e0aaf
Casimira Marshmallows <casimira@at-p2.msk.21-school.ru>
2022-11-15 16:43:34 +0300

    strrchr 100% SAKES

--- src/s21_strrchr.c
+++ src/s21_strrchr.c
@@ -4,11 +4,15 @@ char *s21_strrchr(const char *str, int c) {
   char *arc = s21_NULL;
   int flag = 0;
   s21_size_t i = s21_strlen(str);
-  for (; i > 0; i--) {
-    if (str[i] == c && flag == 0) {
-      arc = (char *)&str[i];
-      flag = 1;
+  if (c != '\0') {
+    for (; i > 0; i--) {
+      if (str[i] == c && flag == 0) {
+        arc = (char *)&str[i];
+        flag = 1;
+      }
     }
+  } else {
+    arc = (char *)&str[i];
   }
   return arc;
 }
