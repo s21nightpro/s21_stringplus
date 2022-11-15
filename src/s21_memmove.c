b4ce45570c7395692cdeda0a19317db0fc0f6b35
Suzi Billye <suzibill@at-p1.msk.21-school.ru>
2022-11-15 16:39:32 +0300

    calloc memmove

--- src/s21_memmove.c
+++ src/s21_memmove.c
@@ -6,7 +6,7 @@ void *s21_memmove(void *dest, const void *src, s21_size_t n) {
   unsigned char *temp = s21_NULL;
 
   if (dest != s21_NULL) {
-    temp = (unsigned char *)malloc(n * sizeof(char));
+    temp = (unsigned char *)calloc(n, sizeof(char));
     if (temp) {
       for (s21_size_t i = 0; i < n; i++) {
         temp[i] = usrc[i];
